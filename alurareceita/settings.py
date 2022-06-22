d5c4e622211bfa7c1f46289b44c1d1424fb878b0
Wellinton B Oliveira <76192977+WellPeres@users.noreply.github.com>
2022-06-22 18:51:25 -0300

    Update settings.py

--- alurareceita/settings.py
+++ alurareceita/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'alura_receita',
         'USER': 'postgres',
-        'PASSWORD': '19202122',
+        'PASSWORD': '123456789',
         'HOST': 'localhost'
     }
 }
